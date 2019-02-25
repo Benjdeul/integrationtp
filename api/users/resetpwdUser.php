7338f9f95dafa8f291ec29b4b954ae68031b933a
Benjdeul <43751383+Benjdeul@users.noreply.github.com>
2019-02-25 16:49:23 +0100

    [WIP]Update API User

--- api/users/resetpwdUser.php
+++ api/users/resetpwdUser.php
@@ -1,19 +1,37 @@
 <?php
 include_once('../../inc/bdd.php');
 $bdd = dbConnect();
-if(isset($_POST['email'])){
+if(isset($_POST['token']) && isset($_POST['newpwd']) && isset($_POST['email'])){
+	$email = htmlspecialchars($_POST['email']);
+	$token = htmlspecialchars($_POST['token']);
+	$newpwd = htmlspecialchars($_POST['newpwd']);
+	$req = $bdd->prepare('SELECT user_username FROM utilisateurs WHERE user_email = ? AND user_tokenreset = ?');
+    $req->execute(array($email, $token));
+    $reqinfo = $req->fetchAll();
+    $exist = $req->rowcount();
+    if($exist > 0){
+    	$req = $bdd->prepare('UPDATE utilisateurs SET user_pwd = ?, user_tokenreset = NULL WHERE user_email = ?');
+   	 	$req->execute(array($newpwd, $email));
+   	 	$req->closeCursor();
+   	 	$success = true;
+        $message = "Mot de passe changé.";
+    }
+}
+else if(isset($_POST['email'])){
 	$email = htmlspecialchars($_POST['email']);
 	$req = $bdd->prepare('SELECT user_username FROM utilisateurs WHERE user_email = ?');
     $req->execute(array($email));
-    $reqinfo = $req->fetchAll();
     $exist = $req->rowcount();
     $req->closeCursor();
     if($exist > 0){
     	$token = openssl_random_pseudo_bytes(16);
 		$token = bin2hex($token);
-    	$req = $bdd->prepare('UPDATE utilisateurs SET user_tokenreset = ? WHERE user_email = ?');
-   	 	$req->execute(array($token, $email));
-   	 	mail($email, 'Reset password', 'http://localhost/tpintegration/resetpassword.php?token='.$token.'&email='.$email);
+		$tmppwd = openssl_random_pseudo_bytes(16);
+		$tmppwd = bin2hex($tmppwd);
+    	$req = $bdd->prepare('UPDATE utilisateurs SET user_tokenreset = ?, user_pwd = ? WHERE user_email = ?');
+   	 	$req->execute(array($token, $tmppwd, $email));
+   	 	$req->closeCursor();
+   	 	//mail($email, 'Reset password', 'http://localhost/tpintegration/resetpassword.php?token='.$token.'&email='.$email);
    	 	$success = true;
         $message = "Email envoyé.";    
    	}
